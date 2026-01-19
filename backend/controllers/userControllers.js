617f97cab0bfedaf2a911c4f73b842ed35a00a80
keshar18 <keshargupta6695@gmail.com>
2026-01-19 19:28:01 +0530

    making profile function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -79,3 +79,16 @@ export const loginUser= async(req,res)=>{
     }
 }
 
+//getting user profile function
+export const getUserProfile= async(req,res)=>{
+    try{
+       const user=await User.findById(req.user._id).select('-password');
+       if(!user){
+        return res.status(404).json({message:"user not found"})
+       }
+       res.status(200).json(user);
+    }
+    catch(error){
+
+    } 
+}
