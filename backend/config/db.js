45fcc3b015a621e4e521ba51df75b55cb20d11ce
keshar18 <keshargupta6695@gmail.com>
2026-01-20 20:53:33 +0530

    change DB code

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try{
-        await mongoose.connect('mongodb+srv://keshargupta6695_db_user:resumebuilder123@cluster0.cqveq1o.mongodb.net/RESUME')
-        .then(()=>{
-            console.log("MongoDB connected successfully");
-        })
-    }catch(error){
-        console.error("Error connecting to MongoDB:", error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully ✅");
+  } catch (error) {
+    console.error("MongoDB connection failed ❌");
+    console.error(error.message);
+    process.exit(1);
+  }
+};
