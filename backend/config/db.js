1ad5d6749ee837a6b73346055ac4c6c8400033b1
keshar18 <keshargupta6695@gmail.com>
2026-01-21 20:01:38 +0530

    error solve

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,6 +7,6 @@ export const connectDB = async () => {
   } catch (error) {
     console.error("MongoDB connection failed ❌");
     console.error(error.message);
-    process.exit(1);
+    process.exit(2);
   }
 };
