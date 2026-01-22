86bd2536b859100f5da82895b6c38e790c9d2584
keshar18 <keshargupta6695@gmail.com>
2026-01-22 20:05:40 +0530

    define info for resume

--- backend/models/resumeModel.js
+++ backend/models/resumeModel.js
@@ -1,3 +1,40 @@
 import mongoose from 'mongoose';
 
-const ResumeSchema= new mongoose.Schema
+const ResumeSchema= new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    thumbnailLink:{
+        type: String,
+    },
+    template:{
+        theme: String,
+        colorPalette: [String],
+    },
+
+    profileInfo:{
+        profilePreviewUrl: String,
+        fullName: String,
+        designation: String,
+        summary: String
+    },
+
+
+    contactInfo:{
+        email: String,
+        phone: String,
+        address: String,
+        linkedIn: String,
+        github: String,
+        website: String
+    },
+
+
+
+   
