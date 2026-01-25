4b5e2f0c8205ce5e26d135e0d9e4066a8e2064b8
keshar18 <keshargupta6695@gmail.com>
2026-01-25 21:01:39 +0530

    creating file filter

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -9,3 +9,18 @@ const storage= multer.diskStorage({
     },
 });
 
+//File Filter
+const fileFilter= (req,file,cb)=>{
+    const allowedTypes=["image/jpeg","image/jpg","image/png"];
+    if(allowedTypes.includes(file.mimetype)){
+        cb(null,true);
+    }
+    else{
+        cb(new Error("Invalid file type. Only JPEG, JPG and PNG are allowed."),false);
+    }
+}
+
+const upload =multer({
+    storage, fileFilter
+})
+export default upload;
