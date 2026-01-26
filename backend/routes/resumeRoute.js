fe28ef4582b1b66498940df9dd6f71020a87e4a8
keshar18 <keshargupta6695@gmail.com>
2026-01-26 18:26:51 +0530

    create resumeRouter file

---
+++ backend/routes/resumeRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { protect } from '../middleware/authMiddleware.js';
+import { createResume, getUserResumes } from '../controllers/resumeController.js';
+
+const resumeRouter = express.Router()
+
+resumeRouter.post('/', protect, createResume)
+resumeRouter.get('/', protect, getUserResumes)
+resumeRouter.put('/:id', protect, updateResume)
+resumeRouter.delete('/:id', protect, deleteResume)
+
+export default resumeRouter;
