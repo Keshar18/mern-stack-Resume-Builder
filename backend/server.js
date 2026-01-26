25c5319aafe72af78ee03ac135314ddbb75cfe27
keshar18 <keshargupta6695@gmail.com>
2026-01-26 18:33:34 +0530

    import resumeRouter in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ import {connectDB} from './config/db.js'
 import userRouter from './routes/userRoutes.js'
 
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+import resumeRouter from './routes/resumeRoute.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 const PORT=4000;
@@ -20,6 +27,16 @@ app.use('/api/users', userRouter);
 
 
 app.use(express.json());
+
+
+app.use(
+    '/uploads',
+    express.static(path.join(__dirname, '/uploads'),{
+        setHeaders: (res, path)=>{
+            res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+        }
+    })
+)
 
 //Routes
 app.get('/', (req,res)=>{
